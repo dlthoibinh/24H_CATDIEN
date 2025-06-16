ee53a5aa6964a17190a76cb5d0710bb09ad4cdab
dlthoibinh <lamdltb@gmail.com>
2025-06-16 08:36:32 +0700

    sw.js

--- sw.js
+++ sw.js
@@ -1,20 +1,15 @@
 const CACHE_NAME = 'form-app-v1';
 const ASSETS = [
-  './',
-  './index.html',
-  './manifest.json',
-  './assets/icon-512.png'
+  '/',
+  '/index.html',
+  '/manifest.json',
+  '/assets/icon-512.png'
 ];
 
-self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
-  );
-});
+self.addEventListener('install', e =>
+  e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(ASSETS)))
+);
 
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request)
-      .then(resp => resp || fetch(event.request))
-  );
-});
+self.addEventListener('fetch', e =>
+  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)))
+);
